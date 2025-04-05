1431db519b7ce6b52ab825e8ca354635242e3519
Agrawal-Sujal <161844562+sujalagrawal2@users.noreply.github.com>
2025-04-05 11:44:59 +0530

    Added autoGenerate in Entity

--- app/src/main/java/com/hexagraph/pattagobhi/Entity/Card.kt
+++ app/src/main/java/com/hexagraph/pattagobhi/Entity/Card.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Card(
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val deckId: Int,
     val question: String,
--- app/src/main/java/com/hexagraph/pattagobhi/Entity/Deck.kt
+++ app/src/main/java/com/hexagraph/pattagobhi/Entity/Deck.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class Deck(
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String
 )
